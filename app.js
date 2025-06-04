53f14f21fef67177ec8dfb5fda9c277f7e159d2c
skulakov <steeldeath152@gmail.com>
2025-06-04 20:16:57 +0300

    test

--- app.js
+++ app.js
@@ -4,5 +4,16 @@ const isProgrammer = true
 
 
 const resultElement = document.getElementById('result')
-console.log(resultElement.textContent)
-resultElement.textContent = 42
+const input1 = documet.getElementById('input1')
+const input2 = documet.getElementById('input2')
+const submitBtn = document.getElementById('submit')
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
+
+
+
+submitBtn.onclick = function () {
+    const sum = Number(input1.Value) + Number(input2.Value)
+    resultElement.textContent = sum
+}
+
